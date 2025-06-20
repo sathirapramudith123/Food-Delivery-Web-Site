d3f336afffb58db38348a0f3ddd55e2b597cc5c9
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-06-20 07:44:54 +0530

    footer css and php file update add a google map

--- index/footer.php
+++ index/footer.php
@@ -24,14 +24,29 @@
       <!-- Contact -->
       <div class="col-md-4 mb-4">
         <h5 class="text-uppercase">Contact Us</h5>
-        <p>Email: support@foodexpress.com</p>
-        <p>Phone: +1 234 567 890</p>
-        <div class="social-icons mt-2">
+        <p>Email: foodexpress@gmail.com</p>
+        <p>Phone: +9447 222 9452</p>
+        <p>Address: No 56 Jaya St, Malabe Town, Sri Lanka</p>
+        <div class="social-icons mt-2 mb-3">
           <a href="#" class="text-white me-3"><i class="bi bi-facebook"></i></a>
           <a href="#" class="text-white me-3"><i class="bi bi-instagram"></i></a>
           <a href="#" class="text-white"><i class="bi bi-twitter-x"></i></a>
         </div>
+        <!-- Google Map Embed -->
+        <div class="map-responsive">
+          <iframe 
+            src="https://www.google.com/maps?q=No%2056%20Jaya%20St,%20Malabe%20Town,%20Sri%20Lanka&output=embed" 
+            width="100%" 
+            height="200" 
+            frameborder="0" 
+            style="border:0;" 
+            allowfullscreen 
+            aria-hidden="false" 
+            tabindex="0">
+          </iframe>
+        </div>
       </div>
+
     </div>
 
     <hr class="bg-white" />
